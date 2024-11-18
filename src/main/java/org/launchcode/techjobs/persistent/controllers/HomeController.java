f9424674a5bae0b5d68bd02391d44f3ccc1065e8
Cypress <kad2321@yahoo.com>
2024-11-18 13:14:21 -0500

    Task 3 complete. All tests pass, can view Job ID in browser

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +27,9 @@ public class HomeController {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
@@ -67,7 +71,16 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-            model.addAttribute()
+
+        Optional<Job> result = jobRepository.findById(jobId);
+        if(result.isEmpty()){
+            model.addAttribute("title", "Invalid Job ID: " + jobId);
+        } else {
+            Job job = result.get();
+            model.addAttribute("title", job.getName());
+            model.addAttribute("job", job);
+        }
+
             return "view";
     }
 
